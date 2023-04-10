03b45d1d563166c8fe5464cffce49e612b5780e3
JuaBue <juigbueno@gmail.com>
2023-04-10 15:44:17 +0200

    Include initial approach to include a thread

--- main_gcc.c
+++ main_gcc.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <conio.h>
 #include <time.h>
+#include <pthread.h>
 
 #include <stdarg.h>
 #include <windows.h>
@@ -13,7 +14,11 @@
 
 #include "ftd2xx.h"
 
+#define FT_BAUDRATE_1M 1000000
+
 static FILE * s_fileHandler = NULL;
+static pthread_mutex_t dataMutex;
+
 
 void fileOpen()
 {
@@ -58,59 +63,74 @@ void fileAppendData(unsigned char * dataRead)
     }
 }
 
+void * fileWriteTask (void * args)
+{
+    unsigned char dataRead = *(unsigned char *)args;
+    fileAppendData(&dataRead);
+}
+
 int main()
 {
     unsigned char read_buf = 0;
-    DWORD numDevs;
+    unsigned char read_buf_copy = 0;
     FT_STATUS ftStatus;
     FT_HANDLE handle;
-    DWORD wBytes;
     DWORD rBytes;
-    FT_STATUS res;
-    LONG comport;
+    pthread_t writeThread;
+    pthread_attr_t threadAttr;
+
 
     /* Configuración FTDI */
-    if(FT_Open(0, &handle) != FT_OK) {
-        puts("Can't open device"); 
-        return 1;
-    }
+    // if(FT_Open(0, &handle) != FT_OK) {
+    //     puts("Can't open device"); 
+    //     return 1;
+    // }
 
-    ftStatus = FT_SetDataCharacteristics(handle, FT_BITS_8, FT_STOP_BITS_1, FT_PARITY_EVEN);
-    if (ftStatus == FT_OK) {
-        printf("FT_SetDataCharacteristics OK\n");
-    }
-    else {
-        printf("FT_SetDataCharacteristics Failed\n");
-    }
+    // ftStatus = FT_SetDataCharacteristics(handle, FT_BITS_8, FT_STOP_BITS_1, FT_PARITY_EVEN);
+    // if (ftStatus == FT_OK) {
+    //     printf("FT_SetDataCharacteristics OK\n");
+    // }
+    // else {
+    //     printf("FT_SetDataCharacteristics Failed\n");
+    // }
 
-    ftStatus = FT_SetBaudRate(handle, 1000000);
-    if (ftStatus == FT_OK) {
-        printf("FT_SetBaudRate OK\n");
-    }
-    else {
-        printf("FT_SetBaudRate Failed\n");
-    }
+    // ftStatus = FT_SetBaudRate(handle, 1000000);
+    // if (ftStatus == FT_OK) {
+    //     printf("FT_SetBaudRate OK\n");
+    // }
+    // else {
+    //     printf("FT_SetBaudRate Failed\n");
+    // }
 
     //Open the file
     fileOpen();
-    printf("Press any key to stop reading\n");  
+    printf("Press any key to stop reading\n"); 
+    pthread_attr_init(&threadAttr);
+    read_buf = 1;
     while(!kbhit())
     {
-        res = FT_Read(handle, &read_buf, 1, &rBytes);
-        if (res != FT_OK)
-        {
-            printf("read failed %d\n", res);
-            FT_Close(handle);
-            return 1;
-        }
-        else
+        read_buf++;
+        Sleep(1000);
+        // ftStatus = FT_Read(handle, &read_buf, 1, &rBytes);
+        // if (ftStatus != FT_OK)
+        // {
+        //     printf("read failed %d\n", ftStatus);
+        //     FT_Close(handle);
+        //     return 1;
+        // }
+        // else
         {
             printf("0x%02X\n", read_buf); 
-            fileAppendData(&read_buf);      
+            read_buf_copy = read_buf;
+            pthread_create(&writeThread, &threadAttr, fileWriteTask, (void *)&read_buf_copy);
+            //fileAppendData(&read_buf);      
         }
     }    
     printf("Read stopped\n");  
+    // Wait thread to finish
+    pthread_join(writeThread, NULL);
+
     fileClose();
-    FT_Close(handle);
+    //FT_Close(handle);
     return 0;
 }
